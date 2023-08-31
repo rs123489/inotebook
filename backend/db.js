e19429f175c403b5f9870f3c44b405308593d4a1
unknown <rprp5804@gmail.com>
2023-08-31 12:25:22 +0530

    gf

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://Production:Kukku%401975@15.207.57.45:27017/Orgzstack?&authSource=admin"
+const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
