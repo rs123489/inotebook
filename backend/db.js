440b97f0d5d1ed484849378f96375f52bd836e06
unknown <rprp5804@gmail.com>
2023-08-31 12:20:35 +0530

    fd

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://Developement:Kukku%401975@15.207.57.45:27017/Orgzstack?&authSource=admin"
+const mongoURI = "mongodb://Production:Kukku%401975@15.207.57.45:27017/Orgzstack?&authSource=admin"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
